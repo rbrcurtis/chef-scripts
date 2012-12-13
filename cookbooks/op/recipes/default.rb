000f7353ccf358d88cce6db1d2839a1f3a6dc59c
Ryan Curtis <rbrcurtis@gmail.com>
2012-12-13 14:13:04 -0500

    complete-ish

--- cookbooks/op/recipes/default.rb
+++ cookbooks/op/recipes/default.rb
@@ -1,19 +1,28 @@
 node['mongodb']['nodename']  = "mongodb" 
 node['mongodb']['isreplica'] = true
 node['mongodb']['replSet']   = "mut8ed"
+# node['mongodb']['bind_ip']   = '127.0.0.1'
 
 package 'git'
 package 'nginx'
 package 'python-software-properties'
+package 'curl'
 
 include_recipe "apt"
 include_recipe "nodejs::install_from_package"
 include_recipe "mongodb-10gen::single"
 include_recipe "rabbitmq"
+include_recipe "redisio::install"
+include_recipe "redisio::enable"
 
 include_recipe "user"
 
 user_account 'ryan' do
+	ssh_keygen false
+	ssh_keys ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMyNDl2Ncc9QkHPGYiJdqTPr70UnyEQq7nJ3ibLlRF8aZBekTJ/ZJnhPOU2tVdGuRp3vafJKxSzyFq7P2a+KQSnGDUA1Z5+hFNF0IzGsfpkd78qFW4AGkmeo+6bTt7jP2uDfogoAzm8YI/Vs11xCmajNWW1pOr2O0guEGcf/Zg81iQZYQAcI8vMcKrjwUi0KYYxtEL8VuB+bsSyudhU+OpFoNNtCRxB2kZ7jBaB5xmw8Oy/h0utm1CiUjQuLh/lQ+gTJcl6oAorUtRoZHGhJBnt3CkeCoiYaTWtODuanPXxatI6M+PsCkRgFBpGvRTBStRDNiAQv3S7raPqwUgNWFV Ryan wednesday@gmail.com']
+end
+
+user_account 'go' do
 	ssh_keygen false
 	ssh_keys ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMyNDl2Ncc9QkHPGYiJdqTPr70UnyEQq7nJ3ibLlRF8aZBekTJ/ZJnhPOU2tVdGuRp3vafJKxSzyFq7P2a+KQSnGDUA1Z5+hFNF0IzGsfpkd78qFW4AGkmeo+6bTt7jP2uDfogoAzm8YI/Vs11xCmajNWW1pOr2O0guEGcf/Zg81iQZYQAcI8vMcKrjwUi0KYYxtEL8VuB+bsSyudhU+OpFoNNtCRxB2kZ7jBaB5xmw8Oy/h0utm1CiUjQuLh/lQ+gTJcl6oAorUtRoZHGhJBnt3CkeCoiYaTWtODuanPXxatI6M+PsCkRgFBpGvRTBStRDNiAQv3S7raPqwUgNWFV Ryan wednesday@gmail.com']
 end
@@ -25,11 +34,60 @@ template "/etc/sudoers" do
 	group "root"
 end
 
-service "sysklogd"
-service "nginx"
+template '/etc/ssh/sshd_config' do
+  source "sshdconfig.erb"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+service 'ssh' do
+	action :restart
+end
+
+service "nginx" do
+	action :start
+end
 
 link "/etc/localtime" do
 	to "/usr/share/zoneinfo/EST"
-	notifies :restart, resources(:service => ["sysklogd", "nginx"]), :delayed
+	notifies :restart, resources(:service => ["nginx"]), :delayed
 	not_if "readlink /etc/localtime | grep -q 'EST$'"
 end
+
+
+## UFW ##
+
+node['firewall']['rules'] = [
+  "http" => {"port"=> "80"},
+  "ssl" => {"port"=> "443"},
+  "api" => {"port"=> "4000"}
+]
+include_recipe "ufw"
+
+## nginx templates
+template '/etc/nginx/sites-enabled/80' do
+  source "nginx-80.erb"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+template '/etc/nginx/sites-enabled/go' do
+  source "nginx-go.erb"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+cookbook_file '/etc/nginx/sites-enabled/default' do
+	action :delete
+end
+
+execute "reload" do
+  command "sudo service nginx reload"
+end
+
+
+
+
--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,14 @@ host="${1:-ryan@172.16.245.132}"
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
+ssh $host "sudo mkdir -p /etc/nginx/certs"
+scp ~/Dropbox/ssh/go.mut8ed.com.* $host:
+ssh $host 'sudo mv ~/go.mut8ed.com.* /etc/nginx/certs/;sudo chown -R root:root /etc/nginx/certs*'
+
 tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
 cd ~/chef &&
 tar xj &&
 sudo bash install.sh'
+
